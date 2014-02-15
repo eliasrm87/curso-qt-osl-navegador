13f486d75679b4d274e8144509ad77f73920ea68
root <root@curso.(none)>
2014-02-15 14:11:22 +0000

    hola

--- Qt-Navegador/mainwindow.cpp
+++ Qt-Navegador/mainwindow.cpp
@@ -6,9 +6,50 @@ MainWindow::MainWindow(QWidget *parent)
     browser_ = new WebBrowser;
     setCentralWidget(browser_);
 
+    mainMenu_ = new QMenuBar(this);
+
+    mnuMarcadores_ = new QMenu(tr("&Marcadores"),this);
+    mainMenu_->addMenu(mnuMarcadores_);
+    setMenuBar(mainMenu_);
+
+    QFile archivo;
+
+    archivo.setFileName("home.txt");
+    if(archivo.open(QFile::ReadOnly))
+    {
+        while(!archivo.atEnd())
+        {
+            mnuMarcadores_->addAction(archivo.readLine());
+        }
+
+        archivo.close();
+    }
+
+
+    connect(browser_ ,SIGNAL(signalMarcador(QString)),this,SLOT(capturadorMarcador(QString)));
 }
 
 MainWindow::~MainWindow()
 {
 
+}
+
+void MainWindow::capturadorMarcador(QString url)
+{
+    QAction* act = mnuMarcadores_->addAction(url);
+    connect(act, SIGNAL(triggered()), this, SLOT(marcadorPulsado()));
+    QFile archivo;
+    archivo.setFileName("home.txt");
+    if(archivo.open(QFile::WriteOnly|QFile::Append))
+    {
+        QTextStream out(&archivo);
+        out<<url<<endl;
+        archivo.close();
+    }
+
+}
+
+void MainWindow::marcadorPulsado()
+{
+    browser_->goTo(((QAction*)QObject::sender())->text());
 }
--- Qt-Navegador/webbrowser.cpp
+++ Qt-Navegador/webbrowser.cpp
@@ -1,49 +1,67 @@
 #include "webbrowser.h"
+#include <QFile>
 
 WebBrowser::WebBrowser(QWidget *parent) :
     QWidget(parent)
 {
-    web_ = new QWebView;
-    address_ = new QLineEdit;
-    refresh_ = new QToolButton;
-    back_ = new QToolButton;
-    forward_ = new QToolButton;
-    home_ = new QToolButton;
-    layout_ = new QGridLayout;
+    //crea e inicializa obejtos
+    web_ = new QWebView;//crea el objeto
+    address_ = new QLineEdit;//crea el objeto
+    refresh_ = new QToolButton;//crea el objeto
+    back_ = new QToolButton;//crea el objeto
+    forward_ = new QToolButton;//crea el objeto
+    home_ = new QToolButton;//crea el objeto
+    addMarcador_ = new QToolButton;//crea el objeto
+    layout_ = new QGridLayout;//crea el objeto
     refresh_->setIcon(QIcon(QPixmap(":/icons/resources/refresh.png")));
     back_->setIcon(QIcon(QPixmap(":/icons/resources/go-previous.png")));
     forward_->setIcon(QIcon(QPixmap(":/icons/resources/go-next.png")));
     home_->setIcon(QIcon(QPixmap(":/icons/resources/go-home.png")));
-    layout_->addWidget(back_,0,0,1,1);
-    layout_->addWidget(forward_,0,1,1,1);
-    layout_->addWidget(home_,0,2,1,1);
-    layout_->addWidget(refresh_,0,3,1,1);
-    layout_->addWidget(address_,0,4,1,1);
-    layout_->addWidget(web_,1,0,1,5);
-    homepage_="http://duckduckgo.com";
-    address_->setText(homepage_);
-    web_->load(homepage_);
+    layout_->addWidget(back_,0,0,1,1);//agregar layout y le da las cordenadas
+    layout_->addWidget(forward_,0,1,1,1);//agregar layout y le da las cordenadas
+    layout_->addWidget(home_,0,2,1,1);//agregar layout y le da las cordenadas
+    layout_->addWidget(refresh_,0,3,1,1);//agregar layout y le da las cordenadas
+    layout_->addWidget(address_,0,4,1,1);//agregar layout y le da las cordenadas
+    layout_->addWidget(addMarcador_,0,5,1,1);//agregar layout y le da las cordenadas
+    layout_->addWidget(web_,1,0,1,6);//agregar layout y le da las cordenadas
+    homepage_="http://duckduckgo.com";//establece una paguina de inicio
+    address_->setText(homepage_);//agregar la paguina a la variable direccion
+    web_->load(homepage_);//cargar la paguina de inicio
     setLayout(layout_);
     setupConnections();
 }
 
 void WebBrowser::setupConnections()
 {
+    //crea las conecciones nesesarias
     connect(address_,SIGNAL(returnPressed()),this,SLOT(onLoad()));
     connect(refresh_,SIGNAL(pressed()),web_,SLOT(reload()));
     connect(forward_,SIGNAL(pressed()),web_,SLOT(forward()));
     connect(back_,SIGNAL(pressed()),web_,SLOT(back()));
     connect(home_,SIGNAL(pressed()),this,SLOT(onHome()));
+    connect(addMarcador_,SIGNAL(pressed()),this,SLOT(emitirMarcador()));
     connect(web_,SIGNAL(urlChanged(QUrl)),this,SLOT(onUrlChange(QUrl)));
     connect(web_,SIGNAL(loadFinished(bool)),this,SLOT(onLoadFinished(bool)));
+
 }
+
 
 void WebBrowser::onLoad()
 {
+
+
     if(!address_->text().startsWith("http://")
             && !address_->text().startsWith("https://")
             && address_->text().length()!=0)
         web_->load("http://"+address_->text());
+    else
+        web_->load(address_->text());
+
+}
+
+void WebBrowser::emitirMarcador()
+{
+    emit signalMarcador(address_->text());
 }
 
 void WebBrowser::onHome()
@@ -58,9 +76,15 @@ void WebBrowser::onUrlChange(QUrl url)
 
 void WebBrowser::onLoadFinished(bool ok)
 {
+
     if(!ok)
         web_->load("https://duckduckgo.com/?q="+address_->text());
-
-
 }
+
+void WebBrowser::goTo(QString url)
+{
+    address_->setText(url);
+    onLoad();
+}
+
 
