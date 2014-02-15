f88253a873158796d5d059adcf0cc6bce4411a2a
curso <curso@curso.(none)>
2014-02-15 14:16:24 +0000

    Proyecto de clase - navegador

--- Qt-Navegador/mainwindow.h
+++ Qt-Navegador/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMenu>
+#include <QMenuBar>
+
 #include "webbrowser.h"
 class MainWindow : public QMainWindow
 {
@@ -11,7 +14,19 @@ public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
 private:
+    //Navegador que se insertará en la ventana.
     WebBrowser *browser_;
+
+    //Barra de menú.
+    QMenuBar *mainMenu_;
+    //Opción Marcadores para la barra de menú.
+    QMenu *mnuMarcadores_;
+
+public slots:
+    void onCapturarSignalAnadirMarcador(QString URL);
+    void onMarcador();
+
+
 };
 
 #endif // MAINWINDOW_H
