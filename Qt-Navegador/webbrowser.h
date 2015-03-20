a2c0085f3282275cf34112399d3ed6b4eaf8a85d
alehdezp <aleyadrihp@gmail.com>
2015-03-20 15:06:02 +0000

    Navegador web

--- Qt-Navegador/webbrowser.h
+++ Qt-Navegador/webbrowser.h
@@ -6,12 +6,14 @@
 #include <QLineEdit>
 #include <QToolButton>
 #include <QWidget>
+#include <QDialog>
+#include <QFile>
 class WebBrowser : public QWidget
 {
     Q_OBJECT
 public:
     explicit WebBrowser(QWidget *parent = 0);
-
+    void pushmarcador();
 private:
     QWebView *web_;
     QLineEdit *address_;
@@ -19,18 +21,25 @@ private:
     QToolButton *back_;
     QToolButton *forward_;
     QToolButton *home_;
+    QToolButton *marcador_;
+    QToolButton *cambiarinicio_;
     QGridLayout *layout_;
     QString homepage_;
+    QDialog *dialog_;
+
 private:
     void setupConnections();
 signals:
-
+void signalMarcador(QString);
 public slots:
     void onLoad();
     void onHome();
     void onUrlChange(QUrl url);
-
+    void marcador();
     void onLoadFinished(bool ok);
+    void cargarmarcador(QString);
+    QString homepage();
+    void changehomepage();
 };
 
 #endif // WEBBROWSER_H
